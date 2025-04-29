861c70bc80a11f5d943d16ec2d9a2d604aa25438
anujgoyaleng <anuj.goyal@kalvium.community>
2025-04-29 22:18:47 +0530

    Bruno Api Testing

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -89,6 +89,7 @@ export const login = async (req, res) => {
       })
       .json({
         message: `Welcome back ${user.fullname}`,
+        token,
         success: true,
       });
   } catch (error) {
