486cb2feabb6c49a2df8bdad31ebfa7349352eb4
anujgoyaleng <anuj.goyal@kalvium.community>
2025-05-30 17:05:39 +0530

    modiy code

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,14 @@
 import express from "express";
 import {login,register, updateProfile, logout } from "../controllers/user.controllers.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(upload.single('file'), register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-router.route("/profile/update").put(isAuthenticated, updateProfile);
+router.route("/profile/update").put(isAuthenticated, upload.single('file'), updateProfile);
 
 export default router;
 
--- client/src/components/auth/Signup.jsx
+++ client/src/components/auth/Signup.jsx
@@ -1,9 +1,161 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast, Toaster } from "react-hot-toast";
+// import Recuiter from "./Recuiter";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [input, setInput] = useState({
+    fullname: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+  const navigate = useNavigate();
 
-export default Signup
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("email", input.email);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+    console.log(input);
+    console.log(formData.get("file"));
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            },  
+            withCredentials: true,
+    });
+    if (res.data.success) {
+        navigate("/login")
+        toast.success(res.data.message)
+    }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <Navbar />
+      <Toaster position="top-center" />
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-5">Sign Up</h1>
+          <div className="my-5">
+            <Label>Full Name</Label>
+            <Input
+              type="text"
+              value={input.fullname}
+              name="fullname"
+              onChange={changeEventHandler}
+              placeholder="Anuj Goyal"
+            />
+          </div>
+          <div className="my-5">
+            <Label>Email</Label>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="AnujGoyal@gmail.com"
+            />
+          </div>
+          <div className="my-5">
+            <Label>Phone Number</Label>
+            <Input
+              type="text"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              placeholder="+91"
+            />
+          </div>
+          <div className="my-5">
+            <Label>Password</Label>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="Password"
+            />
+          </div>
+          <div className="flex item-centre justify-between ">
+            <RadioGroup className="flex items-center gap-4 my-2">
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="student"
+                  checked={input.role === "student"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r1">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <Input
+                  type="radio"
+                  name="role"
+                  value="recuiter"
+                  checked={input.role === "recuiter"}
+                  onChange={changeEventHandler}
+                  className="cursor-pointer"
+                />
+                <Label htmlFor="r2">Recuiter</Label>
+              </div>
+            </RadioGroup>
+            <div className="flex-item-centre gap-2 ">
+              <Label>Profile</Label>
+              <Input
+                accept="image/*"
+                type="file"
+                onChange={changeFileHandler}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <Button type="submit" className=" w-full my-4">
+            SignUp
+          </Button>
+          <span>
+            Already have an account?
+            <Link to="/login" className="text-blue-600">
+              Login
+            </Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
